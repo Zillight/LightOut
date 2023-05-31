695e3df0b000a250383e28d21775220be3bb72cd
Banji Zillight <banjiolawolez@gmail.com>
2023-05-31 18:10:44 +0100

    definining Schema

---
+++ models/campsite.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const CampsiteSchema = new mongoose.Schema({
+  title: String,
+  price: String,
+  description: String,
+  location: String,
+});
+
+module.exports = mongoose.models("Campsite", CampsiteSchema);
