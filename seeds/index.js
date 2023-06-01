c5585c3d30378db8e795898e040a31025af8a3bf
Banji Zillight <banjiolawolez@gmail.com>
2023-06-01 11:11:54 +0100

    loading 50 sample cities from the imported DB

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Campsite = require("../models/campsite");
+const cities = require ('./cities')
 
 mongoose.connect("mongodb://localhost:27017/lightOut", {
   useNewUrlParser: true,
@@ -13,3 +14,16 @@ db.once("open", () => {
   console.log("Database Connected");
 });
 
+const seedDB = async () => {
+  await Campsite.deleteMany({});
+  for (i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const city = cities[random1000];
+    const camp = new Campsite({
+      location: `${city.city}, ${city.state}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB();
